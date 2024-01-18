d7ac9e86358bac920285e2ebbcabbc96d33fe70e
phoebe <mongzhen0856@gmail.com>
2024-01-18 09:04:34 +0800

    wrtie the function max

--- week-2/assignment-1/Function_and_Array.js
+++ week-2/assignment-1/Function_and_Array.js
@@ -1,13 +1,23 @@
 /*
-@max: find the max value of an array of numbers 數位數位的最大值
 @findPosition: find the first position of the target number inside an array of numbers.
                The position should be counted starting from 0, if you can't find the target, please return -1
 在數位數位數位數組中找到目標數位的第一個位置。位置應從0開始計算，如果找不到目標，返回-1
 */
 
 function max(numbers) {
-  // your code here, for-loop method preferred
+  // 宣告一個基準0
+  let maxNumber = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > maxNumber) {
+      // 如果array中的索引值大於基準
+      maxNumber = numbers[i]; // 則基準等於該array中的索引值
+    }
+  }
+
+  return maxNumber;
 }
+
 function findPosition(numbers, target) {
   // your code here, for-loop method preferred
 }
