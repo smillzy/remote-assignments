5886d0b0891afc885082386744c9903975db5e5d
phoebe <mongzhen0856@gmail.com>
2024-02-01 10:59:12 +0800

    Modify again

--- week-3/Assignments/src/main/java/com/remoteassignments/assignments/AssignmentsApplication.java
+++ week-3/Assignments/src/main/java/com/remoteassignments/assignments/AssignmentsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AssignmentsApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(AssignmentsApplication.class, args);
 	}
 
